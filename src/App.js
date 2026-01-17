45af92413ac09a540c8cfe3b5de6283193fc0013
BenGuz2002 <benguz2002@gmail.com>
2026-01-17 01:29:05 -0300

    base del proyecto

---
+++ src/App.js
@@ -0,0 +1,15 @@
+import React from 'react';
+import './App.css';
+
+function App() {
+  return (
+    <div className="App">
+      <header className="App-header">
+        <h1>Proyecto Cencosud</h1>
+        <p>Gestión de Cencosud</p>
+      </header>
+    </div>
+  );
+}
+
+export default App;
